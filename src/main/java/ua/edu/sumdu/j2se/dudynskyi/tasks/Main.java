62e3b0dbf624cede68a7bfa69418a27b954b7683
Александр <dudinckiy@ukr.net>
2022-05-18 12:24:27 +0300

    add class LinkedTaskList and update class Task

--- src/main/java/ua/edu/sumdu/j2se/dudynskyi/tasks/Main.java
+++ src/main/java/ua/edu/sumdu/j2se/dudynskyi/tasks/Main.java
@@ -1,9 +1,6 @@
 package ua.edu.sumdu.j2se.dudynskyi.tasks;
 
-import java.util.Arrays;
-
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("Hello");
     }
