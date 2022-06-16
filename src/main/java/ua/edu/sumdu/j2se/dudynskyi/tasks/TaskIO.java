b9b8449904ecb807e178b8e2716b9639ab11b630
Александр <dudinckiy@ukr.net>
2022-06-16 12:42:29 +0300

    added changes in according to practice_9

--- src/main/java/ua/edu/sumdu/j2se/dudynskyi/tasks/TaskIO.java
+++ src/main/java/ua/edu/sumdu/j2se/dudynskyi/tasks/TaskIO.java
@@ -4,26 +4,81 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.io.*;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.TimeZone;
 
 public class TaskIO {
     public static void write(AbstractTaskList tasks, OutputStream out) {
-        try (ObjectOutputStream oos = new ObjectOutputStream(out)) {
-            oos.writeObject(tasks);
+        try (DataOutputStream dos = new DataOutputStream(out)) {
+            dos.writeInt(tasks.taskAmount);
+            for (Task task : tasks) {
+                char[] title = task.getTitle().toCharArray();
+                dos.writeInt(title.length);
+                dos.writeChars(task.getTitle());
+                dos.writeBoolean(task.isActive());
+                dos.writeInt(task.getRepeatInterval());
+                if (task.isRepeated()) {
+                    long start = dateToLong(task.getStartTime());
+                    dos.writeLong(start);
+                    long end = dateToLong(task.getEndTime());
+                    dos.writeLong(end);
+                } else {
+                    long time = dateToLong(task.getTime());
+                    dos.writeLong(time);
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private static long dateToLong(LocalDateTime time) {
+        ZonedDateTime timeZ = ZonedDateTime.of(time
+                , ZoneId.systemDefault());
+        return timeZ.toInstant().toEpochMilli();
+    }
+
     public static void read(AbstractTaskList tasks, InputStream in) {
-        try (ObjectInputStream objectInputStream = new ObjectInputStream(in)) {
-            AbstractTaskList list = (AbstractTaskList) objectInputStream.readObject();
-            for (Task task : list) {
+        try (DataInputStream dis = new DataInputStream(in)) {
+            int taskAmount = dis.readInt();
+            for (int i = 0; i < taskAmount; i++) {
+                Task task;
+                int length = dis.readInt();
+                char[] chars = new char[length];
+                for (int j = 0; j < length; j++) {
+                    chars[j] = dis.readChar();
+                }
+                String title = String.valueOf(chars);
+                boolean active = dis.readBoolean();
+                int repeatInterval = dis.readInt();
+                if (repeatInterval > 0) {
+                    long startMilli = dis.readLong();
+                    LocalDateTime start = longToDate(startMilli);
+                    long endMilli = dis.readLong();
+                    LocalDateTime end = longToDate(endMilli);
+                    task = new Task(title, start, end, repeatInterval);
+                } else {
+                    long timeMilli = dis.readLong();
+                    LocalDateTime time = longToDate(timeMilli);
+                    task = new Task(title, time);
+                }
+                task.setActive(active);
                 tasks.add(task);
             }
-        } catch (IOException | ClassNotFoundException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    private static LocalDateTime longToDate(long milli) {
+        return LocalDateTime
+                .ofInstant(Instant.ofEpochMilli(milli)
+                        , TimeZone.getDefault().toZoneId());
+    }
+
 
     public static void writeBinary(AbstractTaskList tasks, File file) {
         try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file))
